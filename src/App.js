08b3069d7883ebf76649ffe35cf3f741c47ba5b2
Brian Haack <haack79@hotmail.com>
2020-06-23 22:47:42 -0700

    game is now fully working with speed and word count given

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React, {useState, useEffect, useRef} from "react"
 
 function App() {
     const startTime = 3; 
-    const [text, setText] = useState('');
+    const [text, setText] = useState(" ");
     const [timeLeft, setTimeLeft] = useState(startTime); 
     const [wordCount, setWordCount] = useState(0);
-    const [isRunning, setIsRunning] = useState(false);
+    const [isRunning, setIsRunning] = useState(false); 
     const [speed, setSpeed] = useState(0);  
     const textBoxRef = useRef(null); 
 
@@ -16,37 +16,41 @@ function App() {
 
     const handleChange = (e) => {
       const {value} = e.target;
+      console.log(value, 'this is the valueeeeeeee');
       setText(value); 
     }
 
     const countTheWords = (text) => {
-      const wordsArr = text.trim().split(' ');
-      const totalWords = wordsArr.filter(word => word !== ' ').length; 
+      console.log(text, 'this is the text');
+      const wordsArr = text.trim().split(" ");
+      const totalWords = wordsArr.filter(word => word !== " ").length; 
       setWordCount(totalWords); 
       return totalWords; 
     } 
 
-    const startGame = () => {
-      setText(''); 
+    const startGame = () => { 
       // have to set textboxref disabled to false or it won't let me focus on the textbox area
       // it's a weird bug, not sure how else to handle it. 
       textBoxRef.current.disabled = false;
       textBoxRef.current.focus(); 
       setTimeLeft(startTime); 
       setIsRunning(true); 
+      setText('');
     }
-
+    
     const endGame = () => {
-      setIsRunning(false); 
-      setSpeed(wordsPerSecond())
+      setIsRunning(false);
+      setWordCount(countTheWords(text));
+      setSpeed(wordsPerSecond());  
     }
-
+    // let finalSpeed; 
     useEffect(() => {
       if (timeLeft > 0 && isRunning) {
         setTimeout(() => {
-          setTimeLeft(time => time - 1)}, 1000)
-      } else if (timeLeft === 0) {
-        endGame(); 
+          setTimeLeft(time => time - 1)
+        }, 1000)
+      } else if (timeLeft === 0) { 
+         endGame();
       }
     }, [timeLeft, isRunning])
 
@@ -66,10 +70,10 @@ function App() {
             >
                 Start
             </button>
-            <h1>Word count: {countTheWords(text)}</h1>
+            <h1>Word count: {wordCount}</h1>
             <h1>Speed: {speed}</h1>
         </div>
     )
 }
 
-export default App
+export default App;
