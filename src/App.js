6891291fb9efacc6edaa71635c60a6916933ecf4
Brian Haack <haack79@hotmail.com>
2020-06-23 20:13:09 -0700

    updated styping, functionality, useEffect logic, setRef

--- src/App.js
+++ src/App.js
@@ -5,7 +5,14 @@ function App() {
     const startTime = 3; 
     const [text, setText] = useState('');
     const [timeLeft, setTimeLeft] = useState(startTime); 
-    const [wordCount, setWordCount] = useState(0); 
+    const [wordCount, setWordCount] = useState(0);
+    const [isRunning, setIsRunning] = useState(false);
+    const [speed, setSpeed] = useState(0);  
+    const textBoxRef = useRef(null); 
+
+    const wordsPerSecond = () => {
+      return (wordCount / startTime).toFixed(2); 
+    }
 
     const handleChange = (e) => {
       const {value} = e.target;
@@ -14,34 +21,53 @@ function App() {
 
     const countTheWords = (text) => {
       const wordsArr = text.trim().split(' ');
-      return wordsArr.filter(word => word !== ' ').length; 
+      const totalWords = wordsArr.filter(word => word !== ' ').length; 
+      setWordCount(totalWords); 
+      return totalWords; 
     } 
 
-    useEffect(() => {
-      if (timeLeft > 0) {
-        setTimeout(time => {
-          setTimeLeft(time => time - 1)}, 1000)
-      }
+    const startGame = () => {
+      setText(''); 
+      // have to set textboxref disabled to false or it won't let me focus on the textbox area
+      // it's a weird bug, not sure how else to handle it. 
+      textBoxRef.current.disabled = false;
+      textBoxRef.current.focus(); 
+      setTimeLeft(startTime); 
+      setIsRunning(true); 
+    }
 
-    }, [timeLeft])
+    const endGame = () => {
+      setIsRunning(false); 
+      setSpeed(wordsPerSecond())
+    }
+
+    useEffect(() => {
+      if (timeLeft > 0 && isRunning) {
+        setTimeout(() => {
+          setTimeLeft(time => time - 1)}, 1000)
+      } else if (timeLeft === 0) {
+        endGame(); 
+      }
+    }, [timeLeft, isRunning])
 
     return (
         <div>
             <h1>How fast do you type?</h1>
             <textarea
-                // ref={textBoxRef}
+                ref={textBoxRef}
                 onChange={handleChange}
                 value={text}
-                // disabled={!isTimeRunning}
+                disabled={!isRunning}
             />
             <h4>Time remaining: {timeLeft}</h4>
             <button 
-                // onClick={startGame}
-                // disabled={isTimeRunning}
+                onClick={startGame}
+                disabled={isRunning}
             >
                 Start
             </button>
-            {<h1>Word count: {countTheWords(text)}</h1>}
+            <h1>Word count: {countTheWords(text)}</h1>
+            <h1>Speed: {speed}</h1>
         </div>
     )
 }
